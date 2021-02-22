3f7bd3e37aa33a84ae774988a1e424bfb7e300e6
atoy322 <atoy_0322@yahoo.co.jp>
2021-02-22 23:38:19 +0900

    update

--- gnu.py
+++ gnu.py
@@ -17,7 +17,8 @@ def GetNews():
     """
     page = requests.get(URL).content.decode()
     found_list = re.findall(PATTERN_STRING, page)
-    news_list = map(lambda x: re.sub("<.+?>", "", x), found_list)  # "<h1>Hello</h1>" -> "Hello"
+    #news_list = map(lambda x: re.sub("<.+?>", "", x), found_list)  # "<h1>Hello</h1>" -> "Hello"
+    news_list = [re.sub("<.+?>", "", i) for i in found_list]  # "<h1>Hello</h1>" -> "Hello"
     return list(news_list)
 
 
@@ -28,12 +29,16 @@ if __name__ == "__main__":
         "[0-9]+?月[0-9]+?日": "GREEN",
         "常田大希|井口理|新井和輝|勢喜遊": "YELLOW"
     }  # Pattern: Color
+    
     LIGHTBLACK = "\x1b[100m"  # background color
     END = "\x1b[49m"
 
     news_list = GetNews()
 
     for idx, i in enumerate(news_list):
-        if idx % 2: l = r = ""
-        else: l, r = LIGHTBLACK, END
+        if idx % 2:
+            l = r = ""
+        else:
+            l, r = LIGHTBLACK, END
+        
         cprint(l+"◦ "+i+r, color_table=color_table)
